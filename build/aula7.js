4051c838a8bc1262115757887000b9313bad851e
samuelpd22 <samuel050101@hotmail.com>
2024-02-09 17:53:51 -0300

    studing

---
+++ build/aula7.js
@@ -0,0 +1,6 @@
+"use strict";
+let coisas = ["samuel", 22];
+coisas[1] = 4;
+console.log(coisas);
+console.log(coisas[1]);
+coisas.push();
---
+++ src/aula7.ts
@@ -0,0 +1,7 @@
+let coisas:[string , number]=["samuel",22];// Tuplas, definimos para atribuir depois.
+coisas[1]= 4;//Vai atribuir o valor a posição que informei dentro do Array[]<<<
+
+console.log(coisas);//Imprimi tudo
+console.log(coisas[1]);//Vai imprimir na posição que coloquei dentro do Array[<<<
+
+coisas.push();//Ele permite utilizar os metodos pop,push, unshift e shift!
