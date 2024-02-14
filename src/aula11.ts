b54fe154e9caf40a5ecf4dfb4e3a779951fea6fe
samuelpd22 <samuel050101@hotmail.com>
2024-02-14 16:36:36 -0300

    ParseInt e toString para converção de tipos

--- src/aula11.ts
+++ src/aula11.ts
@@ -8,7 +8,18 @@ uvalor = 11;
 
 nvalor = <number>uvalor;//Associando valor desconhido com um numerico.
 svalor = <string> uvalor;//Afirmação de tipo!
+nvalor = <number> <unknown> svalor; //Converter primeiro para desconhecido para depois em outro tipo.
+
 
 console.log(typeof(nvalor));
 console.log(typeof(svalor));
 console.log(typeof(uvalor));// Vai imprimir do tipo number, por que eu defini!
+
+                    // ### para converter um tipo ### \\
+
+
+nvalor =Number.parseInt(svalor);//Estamos CONVERTENDO OS TIPOS de String para Number
+nvalor = 10;
+svalor = nvalor.toString();//CONVERTENDO O TIPO Number ao tipo String
+
+
