d3ac6781c6ec7c4c50d04314d5c03e18e99eb790
samuelpd22 <samuel050101@hotmail.com>
2024-02-22 19:38:21 -0300

    Desestruturação

---
+++ build/aula35.js
@@ -0,0 +1,22 @@
+"use strict";
+let av = [10, 20, 30, 40];
+let [aa, bb, cc, dd] = av;
+console.log(aa);
+console.log(bb);
+console.log(cc);
+console.log(dd);
+let [a1, b1, c2, d1] = ["Zezin", "lAele", "Armindo", "Cavador"];
+console.log(a1);
+console.log(b1);
+console.log(c2);
+console.log(d1);
+const objv2 = {
+    cor1: "Verde",
+    cor2: "Amarelo",
+    cor3: "Azul",
+    cor4: "Branco"
+};
+let { cor1, cor2, cor3, cor4 } = objv2;
+console.log(cor3);
+console.log(cor1);
+console.log(cor2);
